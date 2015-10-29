3dc927289f6bc8a128b4eca96e4718a38321aefb
Jonas Jurczok <jonas.jurczok@zalando.de>
2015-10-29 16:49:18 +0100

    completed task2

---
+++ site/02_dependencies/src/main/java/de/zalando/mentoring/Main.java
@@ -0,0 +1,10 @@
+package de.zalando.mentoring;
+
+import com.google.common.collect.Lists;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Lists.newArrayList();
+    }
+}
